485a9bbaef5322f9e79ddf2814d9dadaa21f0dd5
tengrui zhang <tengrui zhang>
2017-09-03 00:07:32 +0800

    no message

--- app/Home/Controller/IndexController.class.php
+++ app/Home/Controller/IndexController.class.php
@@ -388,63 +388,58 @@ class IndexController extends CommonController{
     public function lay_upload_img(){
         global $user;
 
-        if($user){
-            $get_file=$_FILES;
-            if($get_file['img']['size']>0||$get_file['img_1']['size']>0||$get_file['img_2']['size']>0||$get_file['sfz_1']['size']>0||$get_file['sfz_2']['size']>0||$get_file['images']['size']>0){
-                $upload = new \Think\Upload();// 实例化上传类
-                $upload->maxSize   =     3145728000 ;// 设置附件上传大小
-                $upload->exts      =     array('zip', 'rar', 'xls','xlsx', 'doc','docx','ppt','pptx','pdf','jpg','png','jpeg','gif');// 设置附件上传类型
-                $upload->uploadReplace  = false;// 存在同名文件是否覆盖
-                $upload->autoSub   =     false;//是否启用子目录保存
-                $upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
-                $upload->savePath  =     'layui/'; // 设置附件上传（子）目录
-                $upload->saveRule  =     ''; // 设置附件上传（子）目录
-                 
-                // 上传文件 
-                $info   =   $upload->upload();
+        $get_file=$_FILES;
+        if($get_file['img']['size']>0||$get_file['img_1']['size']>0||$get_file['img_0']['size']>0||$get_file['img_2']['size']>0||$get_file['sfz_1']['size']>0||$get_file['sfz_2']['size']>0||$get_file['images']['size']>0){
+            $upload = new \Think\Upload();// 实例化上传类
+            $upload->maxSize   =     3145728000 ;// 设置附件上传大小
+            $upload->exts      =     array('zip', 'rar', 'xls','xlsx', 'doc','docx','ppt','pptx','pdf','jpg','png','jpeg','gif');// 设置附件上传类型
+            $upload->uploadReplace  = false;// 存在同名文件是否覆盖
+            $upload->autoSub   =     false;//是否启用子目录保存
+            $upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
+            $upload->savePath  =     'layui/'; // 设置附件上传（子）目录
+            $upload->saveRule  =     ''; // 设置附件上传（子）目录
+             
+            // 上传文件 
+            $info   =   $upload->upload();
 
 
-                if(!$info) {
-                    $data=array();
-                    $data['code']=0;
-                    $data['msg']=$upload->getError();
-                }else{
-                    if($get_file['img']['size']>0){
-                        $img=$info['img']['savename'];
-                    }
-                    if($get_file['img_1']['size']>0){
-                        $img=$info['img_1']['savename'];
-                    }
-                    if($get_file['img_2']['size']>0){
-                        $img=$info['img_2']['savename'];
-                    }
-                    if($get_file['sfz_1']['size']>0){
-                        $img=$info['sfz_1']['savename'];
-                    }
-                    if($get_file['sfz_2']['size']>0){
-                        $img=$info['sfz_2']['savename'];
-                    }
-                    if($get_file['images']['size']>0){
-                        $img=$info['images']['savename'];
-                    }
-
-                    $img_url='/Uploads/layui/'.$img;
-
-                    $data=array();
-                    $data['code']=0;
-                    $data['msg']='success';
-                    $data['data']["src"]=$img_url;
-
-                    //上传到阿里云OSS
-                    oss_upload( '/Uploads/layui/'.$img );
+            if(!$info) {
+                $data=array();
+                $data['code']=0;
+                $data['msg']=$upload->getError();
+            }else{
+                if($get_file['img']['size']>0){
+                    $img=$info['img']['savename'];
+                }
+                if($get_file['img_1']['size']>0){
+                    $img=$info['img_1']['savename'];
+                }
+                if($get_file['img_0']['size']>0){
+                    $img=$info['img_0']['savename'];
+                }
+                if($get_file['img_2']['size']>0){
+                    $img=$info['img_2']['savename'];
+                }
+                if($get_file['sfz_1']['size']>0){
+                    $img=$info['sfz_1']['savename'];
+                }
+                if($get_file['sfz_2']['size']>0){
+                    $img=$info['sfz_2']['savename'];
+                }
+                if($get_file['images']['size']>0){
+                    $img=$info['images']['savename'];
                 }
 
-                echo json_encode($data);
+                $img_url='/Uploads/layui/'.$img;
+
+                $data=array();
+                $data['code']=0;
+                $data['msg']='success';
+                $data['data']["src"]=$img_url;
+
+                //上传到阿里云OSS
+                oss_upload( '/Uploads/layui/'.$img );
             }
-        }else{
-            $data=array();
-            $data['code']=2;
-            $data['msg']='请登录';
 
             echo json_encode($data);
         }
@@ -456,47 +451,40 @@ class IndexController extends CommonController{
     public function lay_upload_file(){
         global $user;
 
-        if($user){
-            if($_FILES['file']['size']>0){
-                $upload = new \Think\Upload();// 实例化上传类               
-                $upload->maxSize   =     314572800000 ;// 设置附件上传大小
-                $upload->exts      =     array('zip', 'rar', 'xls','xlsx', 'doc','docx','ppt','pptx','pdf','jpg','png','jpeg','gif','mp4','mp3','wav','wma');// 设置附件上传类型
-                $upload->uploadReplace  = false;// 存在同名文件是否覆盖
-                $upload->autoSub   =     false;//是否启用子目录保存
-                $upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
-                $upload->savePath  =     'layui/'; // 设置附件上传（子）目录
-                $upload->saveRule  =     ''; // 设置附件上传（子）目录
-                 
-                // 上传文件 
-                $info   =   $upload->upload();
+        if($_FILES['file']['size']>0){
+            $upload = new \Think\Upload();// 实例化上传类               
+            $upload->maxSize   =     314572800000 ;// 设置附件上传大小
+            $upload->exts      =     array('zip', 'rar', 'xls','xlsx', 'doc','docx','ppt','pptx','pdf','jpg','png','jpeg','gif','mp4','mp3','wav','wma');// 设置附件上传类型
+            $upload->uploadReplace  = false;// 存在同名文件是否覆盖
+            $upload->autoSub   =     false;//是否启用子目录保存
+            $upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
+            $upload->savePath  =     'layui/'; // 设置附件上传（子）目录
+            $upload->saveRule  =     ''; // 设置附件上传（子）目录
+             
+            // 上传文件 
+            $info   =   $upload->upload();
 
 
-                if(!$info) {
-                    $data=array();
-                    $data['code']=0;
-                    $data['msg']=$upload->getError();
-                }else{
-                    $file=$info['file']['savename'];
-                    $file_url='/Uploads/layui/'.$file;
+            if(!$info) {
+                $data=array();
+                $data['code']=0;
+                $data['msg']=$upload->getError();
+            }else{
+                $file=$info['file']['savename'];
+                $file_url='/Uploads/layui/'.$file;
 
-                    $data=array();
-                    $data['code']=0;
-                    $data['msg']='success';
-                    $data['data']["src"]=$file_url;
+                $data=array();
+                $data['code']=0;
+                $data['msg']='success';
+                $data['data']["src"]=$file_url;
 
-                    //上传到阿里云OSS
-                    oss_upload( './Uploads/layui/'.$file );
-                }
-
-                echo json_encode($data);
+                //上传到阿里云OSS
+                oss_upload( './Uploads/layui/'.$file );
             }
-        }else{
-            $data=array();
-            $data['code']=2;
-            $data['msg']='请登录';
 
             echo json_encode($data);
         }
+        
         
     }
 
