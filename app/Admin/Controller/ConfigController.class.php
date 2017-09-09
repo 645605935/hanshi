50d27856e261a68027eaf47ba9b97602951f2824
tengrui zhang <tengrui zhang>
2017-09-09 12:24:20 +0800

    no message

--- app/Admin/Controller/ConfigController.class.php
+++ app/Admin/Controller/ConfigController.class.php
@@ -26,6 +26,15 @@ class ConfigController extends AuthController {
 			}else{
 				unset($data['logo']);
 			}
+
+			if($_FILES['erweima']['size']>0){
+				$img=eUpload(1,$_FILES['erweima'],'config');
+                $data['erweima']	=$img['url'];
+			}else{
+				unset($data['erweima']);
+			}
+
+
             if($d->save($data)){
             	cacheConfig(0);
             	$this->success('修改成功');
